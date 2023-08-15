e01f1353555b825a0fd133e405c7acec616ceccf
CoFranso <cofranso.25@gmail.com>
2023-08-15 03:06:43 -0300

    branches update

--- project/applications/branches/models.py
+++ project/applications/branches/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django_timestamps.softDeletion import SoftDeletionModel
+from django_timestamps.timestamps import TimestampsModel
 
 # Create your models here.
+class Branch(SoftDeletionModel, TimestampsModel):
+    """
+    Clase para Sucursales
+        guarda datos generales de una sucursal
+    """
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    open_hs = models.TimeField(verbose_name='Horario de apertura')
+    close_hs = models.TimeField(verbose_name='Horario de cierre')
+
+    def __str__(self) -> str:
+        return f'{self.name}\n- {self.address}\nHorarios: {self.open_hs} - {self.close_hs}'
+    
