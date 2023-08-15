90d98a2741be335f4e940edea161d73a367f9759
CoFranso <cofranso.25@gmail.com>
2023-08-14 22:17:14 -0300

    emplooye update

--- project/applications/employes/views.py
+++ project/applications/employes/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+
+from .models import Employee
+from .forms import EmployeeCreateForm
+from applications.core.forms import PersonForm
+from applications.core.models import Person
+
+from django.views.generic import (CreateView, DetailView, UpdateView, View)
 
 # Create your views here.
+class EmployeeCreateView(UserPassesTestMixin, CreateView):
+    model = Employee
+    template_name = 'employes/employee_create_form.html'
+    form_class = EmployeeCreateForm
+    success_url = reverse_lazy('users_app:reset_password')
+
+    def test_func(self):
+        return self.request.user.is_staff and self.request.user.is_superuser
+
+class EmployeeProfileView(LoginRequiredMixin, DetailView):
+    model = Employee
+    template_name = 'users/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # 'employee' -> Empleado de la pk
+        
+        # Mas detalles para el perfil del empleado:
+        # context['key'] = value
+
+        return context
+    
+
+class EmployeeUpdate_Person(UserPassesTestMixin, UpdateView):
+    model = Person
+    template_name = 'employes/employee_update_form.html'
+    form_class = PersonForm
