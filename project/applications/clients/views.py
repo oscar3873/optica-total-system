72719933f610a0ba87577dff77f2e4d62bd75b0e
CoFranso <cofranso.25@gmail.com>
2023-08-16 12:48:19 -0300

    update client

--- project/applications/clients/views.py
+++ project/applications/clients/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 
-from django.views.generic import (CreateView, View)
+from django.views.generic import (CreateView, View, DetailView)
 from django.views.generic.edit import (FormView,)
 
 from .models import Customer
@@ -15,6 +15,7 @@ from applications.core.models import Person
 class CustomerCreateView(LoginRequiredMixin, FormView):
     form_class = CustomerForm
     template_name = 'customer/new_customer.html'
+    success_url = reverse('core_app:home')
 
     def form_valid(self, form):
         data = copy(form.cleaned_data)
@@ -27,6 +28,6 @@ class CustomerCreateView(LoginRequiredMixin, FormView):
         )
         
         return super().form_valid(form)
+    
 
-    def get_success_url(self):
-        return reverse('core_app:home')
+# class CustomerDetailView(LoginRequiredMixin, DetailView):
