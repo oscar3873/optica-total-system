7f37466bfec51bc6b5c308c97f6b4d8c7a7e1534
CoFranso <cofranso.25@gmail.com>
2023-08-25 01:15:20 -0300

    update | CORE APP | Se trasladaron los cambios funcionales

--- project/applications/core/forms.py
+++ project/applications/core/forms.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django import forms
 
 from project.settings.base import DATE_NOW
@@ -5,45 +6,56 @@ from project.settings.base import DATE_NOW
 from .models import Person
 
 
+################################################################
+def validate_length(field_value, min_length, error_message):
+    """
+    Valida longitud de una cadena.
+        Para nombres, apellidos, direcciones, telefonos, etc.
+        Muestra el error mandado por argumento.
+    """
+    field_value = str(field_value)
+    if field_value and len(field_value) < min_length:
+        raise forms.ValidationError(error_message)
+
+def validate_birth_date(birth_date):
+    """
+    Valida fecha de nacimiento.
+        Debe tener por lo menos 3 meses de vida o no superar los 85 años.
+    """
+    if birth_date and birth_date >= DATE_NOW.date() - timedelta(days=30):
+        raise forms.ValidationError('La fecha establecida no puede registrarse.')
+
+    age_limit = DATE_NOW.date() - timedelta(days=85*365)
+    if birth_date and birth_date <= age_limit:
+        raise forms.ValidationError('La fecha establecida no puede superar los 85 años.')
+################################################################
+
 class PersonForm(forms.ModelForm):
     class Meta:
         model = Person
         fields = '__all__'
 
     def clean_name(self):
-        name = self.cleaned_data['name']
-        if name and len(name) < 3:
-            raise forms.ValidationError("Ingrese un Nombre válido.")
+        name = self.cleaned_data['first_name']
+        validate_length(name, 3, "Ingrese un nombre válido.")
         return name
     
     def clean_last_name(self):
         last_name = self.cleaned_data['last_name']
-        if last_name and len(last_name) < 2:
-            raise forms.ValidationError("Ingrese un Apellido válido.")
+        validate_length(last_name, 3, "Ingrese un apellido válido.")
         return last_name
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data['phone_number']
-        phone_str = str(phone_number)
-        if phone_number and (len(phone_str) < 7):
-            raise forms.ValidationError("Ingrese un número de teléfono válido.")
+        validate_length(phone_number, 9, "Ingrese un número de teléfono válido")
         return phone_number
-
 
     def clean_dni(self):
         dni = self.cleaned_data['dni']
-        if dni and (len(dni) < 8 or '.' in dni):
-            raise forms.ValidationError('Ingrese un DNI válido sin punto de mil (".").')
+        validate_length(dni, 7, "Ingrese un número de DNI válido.")
         return dni
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if email and len(email) > 100:
-            raise forms.ValidationError("El correo electrónico es demasiado largo.")
-        return email
 
     def clean_birth_date(self):
         birth_date = self.cleaned_data['birth_date']
-        if birth_date and birth_date > DATE_NOW:
-            raise forms.ValidationError("La fecha de nacimiento no puede estar en el futuro.")
+        validate_birth_date(birth_date)
         return birth_date
--- project/applications/core/views.py
+++ project/applications/core/views.py
@@ -1,10 +1,22 @@
-#
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 class HomePageView(LoginRequiredMixin , TemplateView):
     template_name = "core/home_page.html"
 
+class CustomUserPassesTestMixin(UserPassesTestMixin):
+    def test_func(self):
+        """
+        Para la verificacion de Administrador
+        """
+        return self.request.user.is_staff
+
+    def handle_no_permission(self):
+        if not self.request.user.is_authenticated:
+            return super().handle_no_permission()
+        else:
+            context = {}
+            return render(self.request, 'users/denied_permission.html', context)
