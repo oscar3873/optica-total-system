b204ab1e8021c4a48aa337001a7bb290fe6b679e
Franco Coppa <cofranso.25@gmail.com>
2023-08-26 18:42:37 +0000

    edit | USERS APP | Se modifico el argumento name por first_name

--- project/applications/users/managers.py
+++ project/applications/users/managers.py
@@ -3,11 +3,11 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager
 
 
-class UserManager(BaseUserManager, models.Manager):
+class UserManager(BaseUserManager):
 
-    def _create_user(self, name, last_name, username, email, password, is_staff, is_superuser, **extra_fields):
+    def _create_user(self, first_name, last_name, username, email, password, is_staff, is_superuser, **extra_fields):
         user = self.model(
-            first_name = name,
+            first_name = first_name,
             last_name = last_name,
             username=username,
             email=email,
@@ -19,17 +19,17 @@ class UserManager(BaseUserManager, models.Manager):
         user.save()
         return user
 
-    def create_user(self, name, last_name, username, email, password, **extra_fields): # Para Empleados
+    def create_user(self, first_name, last_name, username, email, password, **extra_fields): # Para Empleados
         is_staff = False
         is_superuser = False
-        return self._create_user(name, last_name, username, email, password, is_staff, is_superuser, **extra_fields)
+        return self._create_user(first_name, last_name, username, email, password, is_staff, is_superuser, **extra_fields)
 
-    def create_superuser(self, name, last_name, username, email, password, **extra_fields): # Para SuperAdmin (SALTACODE)
+    def create_superuser(self, first_name, last_name, username, email, password, **extra_fields): # Para SuperAdmin (SALTACODE)
         is_staff = True
         is_superuser = True
-        return self._create_user(name, last_name, username, email, password, is_staff, is_superuser, **extra_fields)
+        return self._create_user(first_name, last_name, username, email, password, is_staff, is_superuser, **extra_fields)
     
-    def create_admin(self, name, last_name, username, email, password, **extra_fields): # Para Admin (OPTICA-TOTAL)
+    def create_admin(self, first_name, last_name, username, email, password, **extra_fields): # Para Admin (OPTICA-TOTAL)
         is_staff = True
         is_superuser = False
-        return self._create_user(name, last_name, username, email, password, is_staff, is_superuser, **extra_fields)
+        return self._create_user(first_name, last_name, username, email, password, is_staff, is_superuser, **extra_fields)
