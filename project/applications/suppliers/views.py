29a7337673317ab6778c66087037ac942fbc4a3f
CoFranso <cofranso.25@gmail.com>
2023-08-26 13:19:01 -0300

    edit | SUPPLIER APP ! | Se actualizo view

--- project/applications/suppliers/views.py
+++ project/applications/suppliers/views.py
@@ -1,34 +1,27 @@
 from django.urls import reverse_lazy
-from django.views.generic import (View, CreateView, DeleteView, ListView, DetailView, FormView)
+from django.views.generic import (View, CreateView, DeleteView, ListView, DetailView, FormView, UpdateView)
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import SupplierForm
 from .models import Supplier
 
 # Create your views here.
-class SupplierCreateView(LoginRequiredMixin, FormView):
+class SupplierCreateView(LoginRequiredMixin, CreateView):
     form_class = SupplierForm
     template_name = 'suppliers/supplier_form.html'
     success_url = reverse_lazy('core_app:home')
 
-    def form_valid(self, form):
-        supplier_data = {
-            'user_made': self.request.user,
-            'name': form.cleaned_data['name'],
-            'email': form.cleaned_data['email'],
-            'phone_number': form.cleaned_data['phone_number'],
-        }
-        Supplier.objects.create(**supplier_data)
-        return super().form_valid(form)
-    
+
+class SupplierUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = SupplierForm
+    template_name = 'suppliers/supplier_form.html'
+    success_url = reverse_lazy('core_app:home')
+
 
 class SuppliersListView(LoginRequiredMixin, ListView):
     model = Supplier
     template_name = 'suppliers/supplier_list.html'
     context_object_name = 'suppliers'
-    
-    def get_queryset(self):
-        return Supplier.objects.all()
     
 
 class SupplierDetailView(DetailView):
