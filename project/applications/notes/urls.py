78b83e8c93b0135b3c898380e8ce5eed1717f0ea
Franco Coppa <cofranso.25@gmail.com>
2023-08-26 17:08:38 +0000

    update | NOTE APP | Se agrego urls.py

---
+++ project/applications/notes/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import NoteCreateView
+
+app_name = 'note_app'
+
+
+urlpatterns = [
+    path(
+        'new/',
+        NoteCreateView.as_view(),
+        name='new_note'
+    ),
+    
+]
