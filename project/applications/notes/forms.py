f8d8e3270c6c5f49fe30c1e32fe8e52f3f0057fb
Franco Coppa <cofranso.25@gmail.com>
2023-08-26 17:07:26 +0000

    update | NOTE APP | Se agrego el form.py

---
+++ project/applications/notes/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .models import Note
+from applications.core.forms import validate_length
+
+
+class NoteCreateForm(forms.ModelForm):
+    
+    class Meta:
+        model = Note
+        fields = '__all__'
+        exclude = ['user_made',] 
+
+    def clean_subject(self):
+        subject = self.cleaned_data.get('subject')
+        validate_length(subject, 3, 'El título debe tener al menos 3 carácteres.')
+        return subject
