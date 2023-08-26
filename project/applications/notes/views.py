ce76b21cf7adf37e98e2dad91d0a421bbf8710d9
Franco Coppa <cofranso.25@gmail.com>
2023-08-26 17:05:20 +0000

    edit | NOTE APP | Se quito el loginrequieredmixin para generalizarlo con CustomUserPassesTestMixin

--- project/applications/notes/views.py
+++ project/applications/notes/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-# Create your views here.
+from applications.core.views import CustomUserPassesTestMixin
+from .models import Note
+from .forms import NoteCreateForm
+
+class NoteCreateView(CustomUserPassesTestMixin, FormView):
+    form_class = NoteCreateForm
+    template_name = 'notes/note_form.html'
+    success_url = reverse_lazy('core_app:home')
+
+    def form_valid(self, form):
+        note_data = {
+            'user_made': self.request.user,
+            'subject': form.cleaned_data['subject'],
+            'description': form.cleaned_data['description'],
+            'branch': form.cleaned_data['branch'],
+        }
+        Note.objects.create(**note_data)
+        return super().form_valid(form)
