ef95d5b742b2e74419dd9c6919dee1d6e431a12e
CoFranso <cofranso.25@gmail.com>
2023-08-28 10:52:59 -0300

    update | DASHBOARD APP | Muestra el template del dashboard para que el compa Carlos labure

--- project/project/urls.py
+++ project/project/urls.py
@@ -24,4 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("applications.users.urls")),
+    path("dashboard/", include("applications.dashboard.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
