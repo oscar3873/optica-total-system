1e3bd2d511b6ca2379d2b9043e4688d9efd9a8c8
carlosmorales1908 <carlosmorales1908@gmail.com>
2023-08-29 15:01:38 -0300

    se terminaron de agregar los componentes al dashboard

--- project/applications/dashboard/views.py
+++ project/applications/dashboard/views.py
@@ -1,6 +1,12 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import View, TemplateView
 
 # Create your views here.
 class DashboardView(TemplateView):
-    template_name = 'dashboard/dashboard_home.html'
+    template_name = 'dashboard/reportes_generales.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        contex=super().get_context_data(**kwargs)
+        contex['total_semanal']=20
+        return contex
