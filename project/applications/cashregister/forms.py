e145f9020d3d5a0cbe51c24816d8601e4b29a631
Oscar Vargas <oscarvargas3873@gmail.com>
2023-09-01 12:13:06 -0300

    <feature>(Cashregister app): Se implemento formulario wizard (Sin validacion) para la caja en los templates | se configuro las rutas | se definio la una view basica para que realice esto con formset | se añadieron iconos animados para el frontend | se modifico el base.html para los enlaces | se rediseño el frontend en general involucrado en la caja

--- project/applications/cashregister/forms.py
+++ project/applications/cashregister/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from applications.cashregister.models import CashRegister
+from django.forms import formset_factory
+from applications.cashregister.models import CashRegister, CashRegisterDetail
 
 
 
@@ -39,3 +40,21 @@ class CloseCashRegisterForm(forms.Form):
     class Meta:
         model = CashRegister
         fields = ['final_balance']
+
+class CashRegisterDetailForm(forms.ModelForm):
+    
+    class Meta:
+        model = CashRegisterDetail
+        fields = ['type_method', 'registered_amount', 'counted_amount', 'difference']
+        widgets = {
+            'type_method': forms.Select(
+                attrs={
+                    'class': 'form-control border-0 bg-transparent', 
+                }),
+            'registered_amount': forms.NumberInput(attrs={'class': 'form-control'}),
+            'counted_amount': forms.NumberInput(attrs={'class': 'form-control'}),
+            'difference': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+
+CashRegisterDetailFormSet = formset_factory(CashRegisterDetailForm, extra=0)
