dbbae2d567b1fbf4e716e2eca88b253a803855e4
Javier Salinas <javiersalinasdev@gmail.com>
2023-09-03 18:51:33 -0300

    <feature>(Branches app): Se implemento formularios update y create | Se implemento listado | Se factorizo codigo

--- project/applications/branches/forms.py
+++ project/applications/branches/forms.py
@@ -5,10 +5,32 @@ from applications.core.mixins import ValidationFormMixin
 
 
 class BranchForm(ValidationFormMixin):
+    """
+    Formulario para ingresar una nueva sucursal (?).
+        fields: [name, adress, open_hs, close_hs]
+    """
+    name = forms.CharField( #esto
+        label='Nombre',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'
+                   }
+        )
+    )
+
+    address = forms.CharField( #esto
+        label='Dirección',
+        widget=forms.TextInput(
+            attrs={'class': 'form-control'
+                   }
+        )
+    )
+
     open_hs = forms.TimeField(
-        label='Horario de Apertura',
+        label='Horario de Apetura',
         widget=forms.TimeInput(
-            attrs={'class': 'form-control', 'type': 'time'},
+            attrs={'class': 'form-control',
+                   'type': 'time'
+                   },
             format='%H:%M'
         )
     )
@@ -16,7 +38,9 @@ class BranchForm(ValidationFormMixin):
     close_hs = forms.TimeField(
         label='Horario de Cierre',
         widget=forms.TimeInput(
-            attrs={'class': 'form-control', 'type': 'time'},
+            attrs={'class': 'form-control',
+                   'type': 'time'
+                   },
             format='%H:%M'
         )
     )
@@ -28,7 +52,7 @@ class BranchForm(ValidationFormMixin):
 
     def clean_name(self):
         name = self.cleaned_data['name']
-        self.validate_length(name, 3, "Ingrese una dirección válida.")
+        self.validate_length(name, 3, "Ingrese una dirección válida.") #ingrese un nombre valido
         return name
     
     def clean_address(self):
--- project/applications/branches/views.py
+++ project/applications/branches/views.py
@@ -1,13 +1,17 @@
 from django.urls import reverse_lazy
-from django.views.generic import (FormView, DetailView, UpdateView, View)
+from django.views.generic import (FormView, DetailView, UpdateView, View, ListView) #aqui
 
 from .forms import BranchForm
+from .models import Branch
 
 from applications.core.mixins import CustomUserPassesTestMixin
 
 
 # Create your views here.
 class BranchCreateView(CustomUserPassesTestMixin, FormView):
+    """
+    Crear una nueva sucursal
+    """
     form_class = BranchForm
     template_name = 'branches/branch_form.html'
     success_url = reverse_lazy('core_app:home')
@@ -18,3 +22,23 @@ class BranchCreateView(CustomUserPassesTestMixin, FormView):
         branch.save()
         return super().form_valid(form)
 
+
+####################### UPDATES #####################
+
+class BranchUpdateView(CustomUserPassesTestMixin, UpdateView):
+    model = Branch
+    form_class = BranchForm
+    template_name = 'branches/branch_update.html'
+    success_url = reverse_lazy('core_app:home')
+
+    def form_valid(self, form):
+        form.instance.user_made = self.request.user
+        return super().form_valid(form)
+
+
+################## LIST ######################
+
+class BranchListView(CustomUserPassesTestMixin, ListView):
+    model = Branch
+    template_name = 'branches/branch_list.html'
+    context_object_name = 'branches'
