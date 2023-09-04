c3aecf8e414e919a18f82cd4cc396ae5cc2f7658
CoFranso <cofranso.25@gmail.com>
2023-09-04 17:00:01 -0300

    Se agrego nuevo carpeta/archivo para sonido de notificacion de notas, es esta probando la carga dinamica de objetos relacionados > feature y feature_type

--- project/static/notes/js/notes.js
+++ project/static/notes/js/notes.js
@@ -7,16 +7,8 @@ noteSocket.onmessage = function(e) {
     const message_note = data.message; // Cambiar "notification" por "message"
     
     const notificationElement = document.createElement('div');
-    const notificationSound = document.createElement('audio');
-
     notificationElement.classList.add('notes');
     notificationElement.textContent = message_note;
-
-    notificationSound.src = 'C:/Users/cofra/OneDrive/Escritorio/SaltaCode/Proyects/optica-total/project/static/notes/hgf/audio.mp3';
-    notificationSound.autoplay = true;
-
-    notificationElement.appendChild(notificationSound);
-    console.log(notificationSound);
 
     const notesContainer = document.getElementById('notes-container'); // Usar "notes-container" en lugar de "notifications-container"
     notesContainer.appendChild(notificationElement);
