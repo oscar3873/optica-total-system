594f53d9556bd1095e5c736f81a53b07e9bf155e
CoFranso <cofranso.25@gmail.com>
2023-09-06 21:35:21 -0300

    FEATURE | NOTES APP | Se agrego nuevo modelo Label para etiquetar las notas

--- project/applications/notes/models.py
+++ project/applications/notes/models.py
@@ -3,6 +3,19 @@ from applications.core.models import BaseAbstractWithUser
 from applications.branches.models import Branch
 
 # Create your models here.
+class Label(BaseAbstractWithUser):
+    """
+    Clase para añadir etiquetas de colores a las notas.
+        -label: Nombre de la etiqueta.
+        -color: Color de la etiqueta (hexadecimal).
+    """
+    label = models.CharField(max_length=20)
+    color = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.label} - {self.color}'
+
+
 class Note(BaseAbstractWithUser):
     """
     Clase para Notas para dejar un mensaje de algun instructivo
@@ -14,6 +27,7 @@ class Note(BaseAbstractWithUser):
     subject = models.CharField(max_length=20, blank=False, null=True, verbose_name='Asunto')
     description = models.TextField(max_length=150, blank=False, null=False, verbose_name='Mensaje')
     branch = models.ForeignKey(Branch, on_delete=models.PROTECT, verbose_name='Sucursal')
+    label = models.ForeignKey(Label, on_delete=models.SET_NULL, verbose_name='Label', null=True)
 
     def __str__(self) -> str:
         return (f'Asunto: {self.subject}\n'+
