899acb1944ac767e2d4f2f47dedaf267e2a955f2
Oscar Vargas <oscarvargas3873@gmail.com>
2023-10-10 12:32:58 -0300

    <fix>(Products): Se agrego pestaña actualizar precio, se corrigio base para mostrar sucursal, Se corrigio estructura de pantalla Actualizar precios(Falta completar)

--- project/applications/products/models.py
+++ project/applications/products/models.py
@@ -32,10 +32,10 @@ class Product(BaseAbstractWithUser):
             
     """
     name = models.CharField(max_length=50, blank=False, null=False, verbose_name="Nombre")
-    barcode = models.PositiveBigIntegerField(verbose_name='Codigo de barra', null=True)
+    barcode = models.PositiveBigIntegerField(verbose_name='Codigo', null=True)
     cost_price =models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True, verbose_name="Precio de costo")
     suggested_price =models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True, verbose_name="Precio sugerido")
-    sale_price =models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True, verbose_name="Precio de venta")
+    sale_price =models.DecimalField(max_digits=15, decimal_places=2, null=True, blank=True, verbose_name="Precio")
     description = models.CharField(max_length=250,verbose_name="Descripción")
     stock = models.PositiveSmallIntegerField()
     category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, blank=True, related_name='product_category',verbose_name="Categoria")
