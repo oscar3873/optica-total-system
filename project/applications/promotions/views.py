34314eeafcc3cdbddd945de311eb9b129b7cec75
CoFranso <cofranso.25@gmail.com>
2023-10-17 15:50:05 -0300

    <feature> (Promotion): Se modifico el script

--- project/applications/promotions/views.py
+++ project/applications/promotions/views.py
@@ -19,6 +19,19 @@ class PromotionCreateView(CustomUserPassesTestMixin, FormView):
             for extra_form in formset:
                 if extra_form.is_valid():
                     print('FORMSET UNIT',extra_form.cleaned_data)
+                    promotion = extra_form.save(commit=False)
+                    if promotion.name: # En teoria si tiene name, description, start_date, end_date es porque el formulario es el original (form-0)
+                        name = promotion.name
+                        description = promotion.description
+                        start_date = promotion.start_date
+                        end_date = promotion.end_date
+                    else:
+                        promotion.name = name
+                        promotion.description = description
+                        promotion.start_date = start_date
+                        promotion.end_date = end_date
+                    print(promotion)
+                    # promotion.save()
 
             return super().form_valid(form)
         else:
