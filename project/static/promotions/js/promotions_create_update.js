3aa8bfa8d07d9cc5c01291ce4addbf2c788e1a96
carlosmorales1908 <carlosmorales1908@gmail.com>
2023-10-20 17:35:37 -0300

    FRONT | se eliminó el archivo add_promotions.js y se agregó el promotions_create_update.js

---
+++ project/static/promotions/js/promotions_create_update.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const promotionInput = document.getElementById('id_form-0-type_discount');
+    promotionInput.addEventListener('click', function () {
+        // CAMBIAR AQUI POR EL ID DEL BTN DEL INPUT DE DESCUENTO
+        const discountInput = document.getElementById('id_form-0-start_date');
+        console.log(promotionInput.value);
+        if(promotionInput.value == 'A'){
+            discountInput.setAttribute('disabled', 'true');
+        }
+        else{
+            discountInput.removeAttribute("disabled");
+        }
+    })
+    
+});
