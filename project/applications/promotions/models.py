5e7486ced66bab821945cf16d04e7b64b31d2915
facu añazgo <facuanazgo@hotmail.com>
2023-10-24 16:05:11 -0300

    <fix>(promotions) se corrigio la lista de los productos en una promocion y el redireccionamiento cuando no hay promociones cargadas

--- project/applications/promotions/models.py
+++ project/applications/promotions/models.py
@@ -19,13 +19,13 @@ class Promotion(models.Model):
     Guarda datos generales de una promoción.
     """
 
-    name = models.CharField(max_length=100, verbose_name='Nombre de la Promoción', null=True, blank=True)
+    name = models.CharField(max_length=100, verbose_name='Nombre', null=True, blank=True)
     type_prom = models.ForeignKey(TypePromotion, on_delete=models.CASCADE, verbose_name='Tipo de promoción', null=True, blank=True)
     description = models.CharField(max_length=250, verbose_name='Descripción', null=True, blank=True)
     start_date = models.DateField(verbose_name='Inicio', null=True, blank=True)
     end_date = models.DateField(verbose_name='Fin', null=True, blank=True)
     discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0, verbose_name="Descuento")
-    is_active = models.BooleanField(default=True, verbose_name='Activo')
+    is_active = models.BooleanField(default=False, verbose_name='Activo')
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Sucursal')
 
     def __str__(self):
