705a4aa1a8f703aa1ffedbacacc225e8d7f1f3eb
CoFranso <cofranso.25@gmail.com>
2023-10-30 16:09:31 -0300

    <fix>(Sale PoS): Fix error suma de subtotal

--- project/applications/sales/views.py
+++ project/applications/sales/views.py
@@ -59,6 +59,9 @@ class PointOfSaleView(LoginRequiredMixin, FormView):
         saleform = SaleForm(self.request.POST)
         # payment_methods = PaymentMethodsFormset(self.request.POST)
 
+        branch_actualy = self.request.session.get('branch_actualy') or self.request.user.branch.pk
+        branch_actualy = Branch.objects.get(id=branch_actualy)
+
         if saleform.is_valid():
             sale = saleform.save(commit=False)
             customer = saleform.cleaned_data['customer']
@@ -69,7 +72,7 @@ class PointOfSaleView(LoginRequiredMixin, FormView):
                 messages.error(self.request, "Descuento de venta Inválido. Ingrese solo valores positivos.")
                 return super().form_invalid(form)
 
-        promotions_active = Promotion.objects.filter(is_active=True)
+        promotions_active = Promotion.objects.filter(is_active=True, branch=branch_actualy, deleted_at=None)
         promotional_products = {promotion: [(promotion.discount)] for promotion in promotions_active}
 
         
