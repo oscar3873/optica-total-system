c43012225df670a4de6514dd3ee348a12f07b1ed
carlosmorales1908 <carlosmorales1908@gmail.com>
2023-10-30 22:28:08 -0300

    FRONT | se comento en la view

--- project/applications/dashboard/views.py
+++ project/applications/dashboard/views.py
@@ -21,31 +21,31 @@ class DailyReportsView(TemplateView):
     template_name = 'dashboard/daily_summary.html'
 
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
 
-        branch_actualy = self.request.session.get('branch_actualy') or self.request.user.branch.pk
-        branch_actualy = Branch.objects.get(id=branch_actualy)
+    #     branch_actualy = self.request.session.get('branch_actualy') or self.request.user.branch.pk
+    #     branch_actualy = Branch.objects.get(id=branch_actualy)
 
-        week_date = DATE_NOW.date() - timedelta(days=7)
+    #     week_date = DATE_NOW.date() - timedelta(days=7)
 
-        sale = Sale.objects.filter(created_at__gte=week_date, created_at__lte=DATE_NOW.date(), branch=branch_actualy)
+    #     sale = Sale.objects.filter(created_at__gte=week_date, created_at__lte=DATE_NOW.date(), branch=branch_actualy)
 
-        week_sales = {
-            'lun': [0, 0],
-            'mar': [0, 0],
-            'mie': [0, 0],
-            'jue': [0, 0],
-            'vie': [0, 0],
-            'sab': [0, 0],
-            'dom': [0, 0]
-        }
+    #     week_sales = {
+    #         'lun': [0, 0],
+    #         'mar': [0, 0],
+    #         'mie': [0, 0],
+    #         'jue': [0, 0],
+    #         'vie': [0, 0],
+    #         'sab': [0, 0],
+    #         'dom': [0, 0]
+    #     }
         
-        for day in week_sales:
-            dia_semana = sale.created_at.strftime('%a').lower()
-            week_sales[dia_semana][0] += sale.total
-            week_sales[dia_semana][1] += OrderDetail.objects.filter(sale=sale).count()
+    #     for day in week_sales:
+    #         dia_semana = sale.created_at.strftime('%a').lower()
+    #         week_sales[dia_semana][0] += sale.total
+    #         week_sales[dia_semana][1] += OrderDetail.objects.filter(sale=sale).count()
 
-        context['week_sales'] = week_sales
-        return context
+    #     context['week_sales'] = week_sales
+    #     return context
     
