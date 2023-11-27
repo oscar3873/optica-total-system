794664b838060b92bb810c01f3811e63c8ac5254
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-27 20:37:57 -0300

    fix

--- project/project/settings_prod.py
+++ project/project/settings_prod.py
@@ -175,4 +175,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR/ "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
