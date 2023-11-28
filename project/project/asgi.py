265972516c46501f0fb3501a7006c529eebfa2fc
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-28 14:00:00 -0300

    fix

--- project/project/asgi.py
+++ project/project/asgi.py
@@ -17,7 +17,7 @@ from applications.core.routing import websocket_urlpatterns
 
 # application = get_asgi_application() # Original
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.prod")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -75,11 +75,13 @@ THIRD_PARTY_APPS = (
 
 
 ###########  *CHANNEL PARA NOTIFICACIONES DE NOTAS*  ############## 
+REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379/1")
+
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [REDIS_URL],
         },
     },
 }
