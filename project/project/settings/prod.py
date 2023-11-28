82ee68573341a2afb4af75bb481fb6a35dee9f25
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-27 22:32:27 -0300

    fix

--- project/project/settings/prod.py
+++ project/project/settings/prod.py
@@ -26,9 +26,10 @@ DATABASES = {
 
 # Configuraciones de static y media
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR/ "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_on_heroku.settings(locals())
