176afa1ac24e288a914f5af6dda959603b0f4ebe
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-29 00:40:05 -0300

    fix

--- project/project/asgi.py
+++ project/project/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -19,6 +19,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.prod")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
+django.setup()
 django_asgi_app = get_asgi_application()
 
 from applications.core.routing import websocket_urlpatterns
