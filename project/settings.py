1872d2efb6f4f00d2d474c579938e18a927279de
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-29 17:08:54 -0300

    fix

--- project/settings.py
+++ project/settings.py
@@ -239,10 +239,10 @@ else:
     }
 
 # Configuraciones de static y media
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 #Production media folder
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
