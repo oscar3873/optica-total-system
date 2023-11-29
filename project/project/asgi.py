4e5cb5eb651ec053e2bc32e8dc2112bfe89e14a4
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-29 01:04:58 -0300

    fix

--- project/project/asgi.py
+++ project/project/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-import django
+import sys
 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
