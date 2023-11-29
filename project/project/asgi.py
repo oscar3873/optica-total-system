1f6aa9228acdfeca39d87e15df93a2714e10d63e
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-29 01:03:49 -0300

    fix

--- project/project/asgi.py
+++ project/project/asgi.py
@@ -9,26 +9,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 import django
-import sys
 
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from pathlib import Path
-
-
-# Agregar el directorio del proyecto al PYTHONPATH
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-sys.path.append(BASE_DIR)
-
-
 # application = get_asgi_application() # Original
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.prod")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-django.setup()
+print("PYTHONPATH:", sys.path)
+print("DJANGO_SETTINGS_MODULE:", os.environ.get("DJANGO_SETTINGS_MODULE"))
 django_asgi_app = get_asgi_application()
 
 from applications.core.routing import websocket_urlpatterns
