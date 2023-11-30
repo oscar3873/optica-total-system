d352e0b5923c813041a66964ad8d3c71d6ba4650
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-30 00:34:16 -0300

    Modificacion en MEDIA ROOT

--- project/settings.py
+++ project/settings.py
@@ -249,4 +249,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "media/"
 # Production media folder
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
