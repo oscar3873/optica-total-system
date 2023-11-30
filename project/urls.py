a9306a21e7af6535f7a66fb81627c0f9616fca45
Oscar Vargas <oscarvargas3873@gmail.com>
2023-11-30 00:50:28 -0300

    Modificacion en url respecto a los staticos

--- project/urls.py
+++ project/urls.py
@@ -20,4 +20,8 @@ urlpatterns = [
     path("notifications/", include("notifications.urls")),
     path("sales/", include("sales.urls")),    
     path("promotions/", include("promotions.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
