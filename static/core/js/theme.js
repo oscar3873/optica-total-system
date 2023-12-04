734124df7bbf900851556d7364d243946cad9f27
Carlos <carlosmorales1908@gmail.com>
2023-12-03 23:55:17 -0300

    FRONT | se corrigio el primer grafico de reportes diarios para que muestre hasta las 22hs

--- static/core/js/theme.js
+++ static/core/js/theme.js
@@ -4197,7 +4197,7 @@ var linePaymentChartInit = function linePaymentChartInit() {
     successful: listSuccessfulPayments,
     failed: listUnderpayments
   };
-  var labels = ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00'];
+  var labels = ['8:00', '9:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
 
   if ($echartsLinePaymentChart) {
     var userOptions = utils.getData($echartsLinePaymentChart, 'options');
