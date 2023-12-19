09775082a48542cce70cb49f3ce294dd1397e125
Oscar Vargas <oscarvargas3873@gmail.com>
2023-12-18 22:35:07 -0300

    <fix>(settings, Proyecto en Gral.):Se corrigio el formato de los numeros Ej. 12345,00 a 12.345,00

--- static/cashregister/js/difference_amount.js
+++ static/cashregister/js/difference_amount.js
@@ -1,25 +1,36 @@
 
-// Obtén una lista de todos los campos de monto manual
+// Lista de todos los campos de monto manual
 const countedAmountFields = Array.from(document.getElementsByClassName('difference-input'));
 
 
+function parseFormattedNumber(numberString) {
+    // Reemplaza el separador de miles y luego el separador decimal para obtener un formato flotante
+    return parseFloat(numberString.replace(/\./g, '').replace(/,/, '.'));
+}
+
+function formatNumberAsCurrency(number) {
+    // Formatea el número como decimal sin símbolo de moneda
+    const formatter = new Intl.NumberFormat('es', {
+        style: 'decimal',
+        minimumFractionDigits: 2, // Para mantener dos decimales
+    });
+    return formatter.format(number);
+}
+
 // Función para calcular y actualizar la diferencia
 function updateDifferences() {
-    countedAmountFields.forEach((currentValue, index, array) => {
-
-        const systemAmount = parseFloat(document.getElementById(`${index+1}`).textContent.replace('$', '').replace(',', ''));
-        const countedAmount = parseFloat(currentValue.value === ''? 0: currentValue.value);
+    countedAmountFields.forEach((currentValue, index) => {
+        const systemAmountString = document.getElementById(`${index+1}`).textContent.replace('$', '').trim();
+        const systemAmount = parseFormattedNumber(systemAmountString);
+        const countedAmount = parseFormattedNumber(currentValue.value === '' ? '0' : currentValue.value);
         const typeMethod = document.getElementById(`${index+1}`).dataset.typeMethod;
         const difference = isNaN(countedAmount) ? null : countedAmount - systemAmount;
         const differenceField = document.getElementById(`difference-${typeMethod}`);
+
         if (difference === null) {
             differenceField.textContent = 'No válido';
         } else {
-            const formatter = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: 'USD',
-            });
-            differenceField.textContent = formatter.format(difference);
+            differenceField.textContent = formatNumberAsCurrency(difference);
         }
     });
 }
